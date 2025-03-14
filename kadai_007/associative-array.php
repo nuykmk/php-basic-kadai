5c0c881520c330f41979db14dbf505a5980bd562
nuykmk <yy.uni.yy.yy@gmail.com>
2025-03-14 15:04:38 +0900

    add kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP課題_007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $item_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    print_r($item_data)
+    ?>
+  </p>
+</body>
+</html>
